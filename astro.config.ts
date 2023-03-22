55dfd2ebad5204ce4d8c183be9c170471b72600e
madson7 <madson.adm7@gmail.com>
2023-03-22 08:26:07 -0300

    init

---
+++ astro.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import codeTitle from "remark-code-title";
+import fs from "node:fs";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), image(), mdx()],
+  site: "https://www.madsonramos.com",
+  markdown: {
+    remarkPlugins: [codeTitle],
+    shikiConfig: {
+      theme: "dark-plus",
+    },
+  },
+  vite: {
+    plugins: [rawFonts([".ttf", ".woff"])],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+});
+
+// vite plugin to import fonts
+function rawFonts(ext: string[]) {
+  return {
+    name: "vite-plugin-raw-fonts",
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        };
+      }
+    },
+  };
+}
---
+++ src/data/socials.ts
@@ -0,0 +1,2 @@
+export const author = "Madson Ramos";
+export const userGitHub = "madson7";
